48f99037aad7c3b2b74f71251d8d2a2b27f1d6d7
Foyzul Karim <foyzulkarim@gmail.com>
2024-04-07 19:20:31 +1000

    Revert "Setup server, Enhance Request Tracking, Error Handling, and Logging"

--- client/storybook-static/assets/Header.stories-58a1c72b.js
+++
@@ -1,7 +0,0 @@
-import{H as g}from"./Header-9dcd8a15.js";import"./jsx-runtime-d079401a.js";import"./index-f1f2c4b1.js";import"./index-d6b32626.js";import"./Button-ec8dc0e4.js";const i={title:"Example/Header",component:g,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:()=>console.log("User logged in"),onLogout:()=>console.log("User logged out"),onCreateAccount:()=>console.log("Account created")}},e={args:{user:{name:"Jane Doe"}}},o={};var r,s,a;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(a=(s=e.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};var t,n,c;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(c=(n=o.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const L=["LoggedIn","LoggedOut"];export{e as LoggedIn,o as LoggedOut,L as __namedExportsOrder,i as default};
--- client/storybook-static/assets/index-72020463.js
+++
@@ -1,1 +0,0 @@
-import{M as s,u as m}from"./index-5042827c.js";import"./index-f1f2c4b1.js";export{s as MDXProvider,m as useMDXComponents};
